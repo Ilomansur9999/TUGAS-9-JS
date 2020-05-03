d9ca40cfd7dcdd1105ac730ea893c74617ab3953
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 16:32:55 +0700

    TUGAS 9 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas9.js
@@ -0,0 +1,17 @@
+function onepiece(){
+var mugiwara = {
+	Kapten : "Luffy",
+	wakil : "Zoro",
+	Navigator : "Nami",
+	Koki : "Sanji",
+	Dokter : "Chopper",
+	}
+
+for(var x in mugiwara){
+	console.log(mugiwara[x])
+}
+}
+
+onepiece()
+
+
